ccad2ae80ad1a8747f2d1d4f03098a5fe58cec75
cristianmorenor <cristian.moreno-r@mail.escuelaing.edu.co>
2026-09-22 11:01:14 -0500

    Mappers de Plato con MapStruct - Semana 8

--- src/main/java/com/restaurante/mapper/PlatoEntityMapper.java
+++ src/main/java/com/restaurante/mapper/PlatoEntityMapper.java
@@ -1,4 +1,11 @@
 package com.restaurante.mapper;
 
-public class PlatoEntityMapper {
+import com.restaurante.model.domain.Plato;
+import com.restaurante.model.dto.response.PlatoResponseDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PlatoEntityMapper {
+
+    PlatoResponseDTO toDTO(Plato plato);
 }
--- src/main/java/com/restaurante/mapper/PlatoMapper.java
+++ src/main/java/com/restaurante/mapper/PlatoMapper.java
@@ -1,4 +1,14 @@
 package com.restaurante.mapper;
 
-public class PlatoMapper {
+import com.restaurante.model.domain.Plato;
+import com.restaurante.model.dto.request.PlatoRequestDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PlatoMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "disponible", constant = "true")
+    Plato toDomain(PlatoRequestDTO dto);
 }
